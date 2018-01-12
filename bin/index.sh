e99729900f1139f696bb1542e022fb5b918a5519
Jurung Park <jurung@pplink.net>
2018-01-12 10:47:56 +0900

    90% done...

--- bin/index.sh
+++ bin/index.sh
@@ -4,5 +4,10 @@ HTML_PATH=$1
 
 echo "@@@ start to make a js file from given HTML file."
 
-# make dom-map.json from the HtML file
-node ../src/html-to-json.js
+# make dom-map.json from the HTML file
+node /usr/local/lib/node_modules/one-src/src/html-to-json.js "`cat $1`"
+
+# make js file by combining dom-map.json and dom-builder.min.js
+echo "var __one_src_map = `cat /tmp/dom-map.json`;`cat /usr/local/lib/node_modules/one-src/src/dom-builder.min.js`" > ./result.js
+
+
